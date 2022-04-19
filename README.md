83f553d7df18605a85a74ae12a65730979064fd4
eliasanta <eliasanta93@gmail.com>
2022-04-19 15:15:06 +0200

    read me

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # discover_the_world
+Readme
+
+I was inspired by a youtube project, and I have add the feauture to see the map
+of the selected country.
+Every time that you restart the page will appear a random coutry or you can choose it by the select box and see some features about that one and the respective flag.
+Try it!
+
+To clone the project: https://github.com/eliasanta/discover_the_world.git
+
+Table of content:
+Version 1.0
+Contributors:Elia
+
+References:
+Choose an open source license. Github. Available at: https://choosealicense.com/
+Getting started with writing and formatting on Github. Github. Available at: https://help.github.com/articles/getting-started-with-writing-and-formatting-on-github/
+
+if you want contact me 
+eliasanta@live.it
+
--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const countriesList= document.getElementById('countries')
 let countries;//conterrà fetched data
 
-
+//************fetch section */
 fetch('https://restcountries.com/v3.1/all')
 .then(res=> res.json())//estraggo il json body
 .then((data)=> initialize(data))//con i dati trasformati in json uso la funzione per inserirli in countries
 .catch((err)=>{console.log('we have a problem',err)})
+//******************* */
 
 //event listener for countries list
 countriesList.addEventListener('change',event => displayCountryInfo(event.target.value));//quando cambio la selezione avvio la funzione
 
-function initialize(countriesData){//questa funzione viener richiamata dal fetch
-    countries=countriesData;//metto i dati su una variabile
-    function SortArray(x, y){//riordino l'oggetto con la funzione
+function initialize(countriesData){//this function is recall by fetch
+    countries=countriesData;//put fetch data in a variable
+    function SortArray(x, y){//reorder fetch data
         return x.name.common.localeCompare(y.name.common);
     }
     var orderCountry = countries.sort(SortArray);
     let options="";
-     /*metodo alternativo for(elem of orderCountry){
+     /*alternative method to display the data
+      for(elem of orderCountry){
    options +=`<option value="${elem.cca2}">${elem.name.common} ${elem.callingcode[0]}</option>`
     }  */
     orderCountry.forEach(elem => {
       options +=`<option value="${elem.cca2}">${elem.name.common}</option>`
     });
     countriesList.innerHTML=options;
-    
-    countriesList.selectedIndex = Math.floor(Math.random()*countriesList.length);//scelgo come display di partenza un paese random
+    //***here i want chose like a initial display a random Country */
+    countriesList.selectedIndex = Math.floor(Math.random()*countriesList.length);
     displayCountryInfo(countriesList[countriesList.selectedIndex].value);
 }
 
@@ -39,6 +41,7 @@ function displayCountryInfo(countryByCca2){
     document.getElementById('population').innerHTML=`${countryData.population} people`;
     document.getElementById('region').innerHTML=countryData.region;
     document.getElementById('sub-region').innerHTML=countryData.subregion;    
+    //Display the capital of the country in the map section
     document.getElementById('gmap_canvas').src=`https://maps.google.com/maps?q=${countryData.capital}&t=&z=13&ie=UTF8&iwloc=&output=embed`
     
   }
